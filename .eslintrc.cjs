b5f1f1e2f58ac14bd13f34070b7a7e88a13074bf
Катерина Юношева <katerinayunosheva@gmail.com>
2024-07-15 12:23:21 +0300

    Настроены файлы конфигурации

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,53 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true, 
+  },
+  parser: '@typescript-eslint/parser', // Использование парсера для TypeScript
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-refresh', '@typescript-eslint'], // Подключение плагинов
   rules: {
+    // ESLint recommended rules
+    'no-console': 'warn', // Предупреждение при использовании console.log()
+    'no-unused-vars': 'error', // Ошибка при обнаружении неиспользуемых переменных в JavaScript
+    'no-undef': 'error', // Ошибка при использовании неопределенных переменных в JavaScript
+    'no-empty': 'warn', // Предупреждение при обнаружении пустых блоков
+    'no-constant-condition': 'warn', // Предупреждение при использовании констант в условиях
+    'no-debugger': 'warn', // Предупреждение при использовании debugger
+
+    // TypeScript-specific rules
+    '@typescript-eslint/no-unused-vars': 'error', // Ошибка при обнаружении неиспользуемых переменных в TypeScript
+    '@typescript-eslint/no-explicit-any': 'error', // Ошибка при использовании any в TypeScript
+
+    // React-specific rules
+    'react/prop-types': 'off', // Отключение проверки PropTypes, так как используется TypeScript
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+
+    // React Hooks rules
+    'react-hooks/rules-of-hooks': 'error', // Проверка соблюдения правил использования React Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Предупреждение о неправильном использовании зависимостей в useEffect
+
+    // Prettier integration
+    'prettier/prettier': 'error', // Ошибка, если код не соответствует правилам Prettier
+
+    // React Refresh rule
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
-    ],
+    ], // Предупреждение, если экспортируется что-то, кроме React-компонентов
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- README.md
+++ README.md
@@ -1,31 +1,17 @@
-# React + TypeScript + Vite
+# Tech Support Management System
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+The Tech Support Management System is a comprehensive solution designed to streamline technical support operations, enhance employee management, and provide a centralized knowledge base for your organization.
 
-Currently, two official plugins are available:
+Start the development server:
+   yarn dev
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Technologies and Tools
 
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
-
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+Framework: React.js
+Bundler: Vite
+Language: TypeScript
+Linting: ESLint
+Formatting: Prettier
+Node.js: v16.x
+Package Manager: Yarn
 
